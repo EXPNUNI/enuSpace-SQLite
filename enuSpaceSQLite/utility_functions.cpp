e80d7857a7547be6f0a9ec975c73a4206b626f07
BrightJoy <jwlee@enu-tech.co.kr>
2017-07-25 18:54:56 +0900

    update

--- enuSpaceSQLite/utility_functions.cpp
+++ enuSpaceSQLite/utility_functions.cpp
@@ -110,3 +110,14 @@ int GetArrayIndexFromDimension(CString strOrgDim, CString strDimension)
 
 	return iIndex;
 }
+
+std::string ReplaceAll(std::string &str, const std::string& from, const std::string& to) 
+{
+	size_t start_pos = 0; 
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos)  
+	{
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); 
+	}
+	return str;
+}
